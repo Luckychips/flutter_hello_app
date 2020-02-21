5ae8e298d428ce9a637f8f9de063ab10ea020c71
todd.kim <sksms20000@gmail.com>
2020-02-21 15:24:58 +0900

    구글맵 샘플소스 코드 추가(ios)

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
-import '../main_navbar.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   static const String id = 'search_screen';
 
   @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<SearchScreen> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('SEARCH')
-      )
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
     );
   }
 }
