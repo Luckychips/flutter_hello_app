50b4fb77d8fe65e12711b9a5483bc8608dc24bb0
todd.kim <sksms20000@gmail.com>
2020-03-23 15:16:33 +0900

    색상변경 기본 애니메이션 처리.

--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
 
-class FeedScreen extends StatelessWidget {
+
+class FeedScreen extends StatefulWidget {
+  @override
+  TransformPager createState() {
+    return new TransformPager();
+  }
+}
+
+class TransformPager extends State<FeedScreen> {
   static const String id = 'feed_screen';
+
+  var backgroundColor = Colors.pink;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('FEED')));
+    return Scaffold(
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AnimatedContainer(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height - 83,
+              duration: Duration(seconds: 1),
+              decoration: BoxDecoration(
+                  color: backgroundColor
+              ),
+              child: Center(
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)
+                    ),
+                    color: Colors.teal,
+                    padding: EdgeInsets.all(15.0),
+                    child: Text('transform',
+                        style: TextStyle(fontSize: 18, color: Colors.white)),
+                    onPressed: () {
+                      setState(() {
+                        if (backgroundColor == Colors.pink) {
+                          backgroundColor = Colors.lightGreen;
+                        } else {
+                          backgroundColor = Colors.pink;
+                        }
+                      });
+                    },
+                  )
+              )
+          ),
+        ],
+      )),
+    );
   }
 }
